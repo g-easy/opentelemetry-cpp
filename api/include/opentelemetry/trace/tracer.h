60dbc90c6fa351b340d9cc179c79e9838a587abf
Bryan Clement <bclement@newrelic.com>
2020-01-07 13:15:45 -0800

    don't dedupe tracers, don't use stl types in public methods

--- api/include/opentelemetry/trace/tracer.h
+++ api/include/opentelemetry/trace/tracer.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "opentelemetry/nostd/string_view.h"
+
+using opentelemetry::nostd::string_view;
+
 namespace opentelemetry
 {
 namespace trace
@@ -7,7 +11,10 @@ namespace trace
 class Tracer
 {
 public:
-  Tracer();
+  Tracer(const string_view&, const string_view&);
+private:
+  const string_view name;
+  const string_view version;
 };
 }  // namespace trace
 }  // namespace opentelemetry
