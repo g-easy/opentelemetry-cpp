3011f2bc8201d2e98e963d36164cd186187ed1f0
easy <g-easy@users.noreply.github.com>
2020-02-25 11:58:47 +1100

    GetTracerProvider() by itself is enough to show the problem.

--- example/lib1.cc
+++ example/lib1.cc
@@ -3,7 +3,7 @@
 void f1() {
   opentelemetry::trace::TracerProvider* p =
     opentelemetry::trace::Provider::GetTracerProvider();
-  opentelemetry::trace::Tracer* const t = p->GetTracer("lib1", "0.0.1");
-  opentelemetry::nostd::unique_ptr<opentelemetry::trace::Span> s = t->StartSpan("Span1");
+  //opentelemetry::trace::Tracer* const t = p->GetTracer("lib1", "0.0.1");
+  //opentelemetry::nostd::unique_ptr<opentelemetry::trace::Span> s = t->StartSpan("Span1");
   //s->AddEvent(); // pending PR35
 }
--- example/lib2.cc
+++ example/lib2.cc
@@ -3,6 +3,6 @@
 void f2() {
   opentelemetry::trace::TracerProvider* p =
     opentelemetry::trace::Provider::GetTracerProvider();
-  opentelemetry::trace::Tracer* const t = p->GetTracer("lib2", "0.0.1");
-  opentelemetry::nostd::unique_ptr<opentelemetry::trace::Span> s = t->StartSpan("Span2");
+  //opentelemetry::trace::Tracer* const t = p->GetTracer("lib2", "0.0.1");
+  //opentelemetry::nostd::unique_ptr<opentelemetry::trace::Span> s = t->StartSpan("Span2");
 }
